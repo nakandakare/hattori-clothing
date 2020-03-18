196b4901dce7bc6d438455fec430f8baf5a2bf88
Kevin <kevin.nakandakare@gmail.com>
2020-03-18 03:04:45 -0300

    added remove functionality to checkout-item.component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import addItemToCart from './cart.util.js';
+import { addItemToCart, removeItemFromCart} from './cart.util.js';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -22,6 +22,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 cartItems: state.cartItems.filter(cartItem => cartItem.id != action.payload.id) //keep 'true' items
+            }
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems,action.payload)
             }
         default: 
             return state;
